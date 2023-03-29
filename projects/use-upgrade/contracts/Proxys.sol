8e87651edf6ab7b3aba0c4bfd10e12b5d7509ec2
sichengwen <sichengwen@kuaishou.com>
2023-03-29 23:38:38 +0800

    beacon proxy

--- projects/use-upgrade/contracts/Proxys.sol
+++ projects/use-upgrade/contracts/Proxys.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
@@ -13,5 +15,15 @@ contract TransparentProxy is TransparentUpgradeableProxy {
         bytes memory _data
     ) payable TransparentUpgradeableProxy(_logic, admin_, _data) {
 
+    }
+}
+
+contract MyBeaconProxy is BeaconProxy {
+    constructor(address beacon, bytes memory data) payable BeaconProxy(beacon, data) {
+    }
+}
+
+contract MyUpgradeableBeacon is UpgradeableBeacon {
+    constructor(address implementation_) UpgradeableBeacon(implementation_) {
     }
 }
---
+++ projects/use-upgrade/test/BeaconProxyTest.ts
@@ -0,0 +1,31 @@
+import {time, loadFixture} from "@nomicfoundation/hardhat-network-helpers";
+import {expect} from "chai";
+import {ethers} from "hardhat";
+
+describe("Lock", function () {
+
+    async function deployLogicFixture() {
+        // Contracts are deployed using the first signer/account by default
+        const [owner, otherAccount] = await ethers.getSigners();
+
+        const Logic = await ethers.getContractFactory("Logic2");
+        const logic = await Logic.deploy();
+
+        const MyUpgradeableBeacon = await ethers.getContractFactory("MyUpgradeableBeacon");
+        const upgradeableBeacon = await MyUpgradeableBeacon.deploy(logic.address);
+
+        const MyBeaconProxy = await ethers.getContractFactory("MyBeaconProxy");
+        const beaconProxy1 = await MyBeaconProxy.deploy(upgradeableBeacon.address, "0x");
+        const beaconProxy2 = await MyBeaconProxy.deploy(upgradeableBeacon.address, "0x");
+
+        return {owner, otherAccount, beaconProxy1, beaconProxy2, Logic};
+    }
+
+    describe("Deployment", function () {
+        it("Should set the right counter", async function () {
+            const {owner, otherAccount, beaconProxy1, beaconProxy2, Logic} = await loadFixture(deployLogicFixture);
+            expect(await Logic.attach(beaconProxy1.address).connect(otherAccount).increament()).to.equal(1);
+            expect(await Logic.attach(beaconProxy2.address).connect(otherAccount).current()).to.equal(0);
+        });
+    });
+});
